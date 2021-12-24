0ac0f3061bb200a6f997f407df544d1b0f20a406
Patrick Wildt <patrick@blueri.se>
2021-12-24 01:18:59 +0100

    cleanup

--- sys/dev/fdt/tpstc.c
+++ sys/dev/fdt/tpstc.c
@@ -60,6 +60,7 @@
 #define  TPS_SYSTEM_POWER_STATE_S4		0x04
 #define  TPS_SYSTEM_POWER_STATE_S5		0x05
 #define TPS_STATUS			0x1a
+#define  TPS_STATUS_PLUG_PRESENT		(1 << 0)
 #define TPS_SYSTEM_CONF			0x28
 #define TPS_CTRL_CONF			0x29
 #define TPS_POWER_STATUS		0x3f
@@ -71,30 +72,6 @@
 #define TPS_TASK_TIMEOUT		0x01
 #define TPS_TASK_REJECTED		0x03
 
-enum typec_cc_status {
-	TYPEC_CC_OPEN,
-	TYPEC_CC_RA,
-	TYPEC_CC_RD,
-	TYPEC_CC_RP_DEF,
-	TYPEC_CC_RP_1_5,
-	TYPEC_CC_RP_3_0,
-};
-
-enum typec_data_role {
-	TYPEC_DEVICE,
-	TYPEC_HOST,
-};
-
-enum typec_power_role {
-	TYPEC_SINK,
-	TYPEC_SOURCE,
-};
-
-enum typec_polarity {
-	TYPEC_POLARITY_CC1,
-	TYPEC_POLARITY_CC2,
-};
-
 struct tpstc_softc {
 	struct device		 sc_dev;
 	i2c_tag_t		 sc_tag;
@@ -103,14 +80,6 @@ struct tpstc_softc {
 	void			*sc_ih;
 
 	struct task		 sc_task;
-
-	int			 sc_attached;
-	enum typec_data_role	 sc_try_data;
-	enum typec_power_role	 sc_try_power;
-
-	uint32_t		*sc_ss_sel;
-	uint8_t			 sc_cc;
-	uint8_t			 sc_vbus_det;
 };
 
 int	 tpstc_match(struct device *, void *, void *);
@@ -119,12 +88,6 @@ int	 tpstc_detach(struct device *, int);
 
 int	 tpstc_intr(void *);
 void	 tpstc_task(void *);
-void	 tpstc_cc_change(struct tpstc_softc *);
-void	 tpstc_power_change(struct tpstc_softc *);
-void	 tpstc_set_polarity(struct tpstc_softc *, int);
-void	 tpstc_set_vbus(struct tpstc_softc *, int, int);
-void	 tpstc_set_roles(struct tpstc_softc *, enum typec_data_role,
-	    enum typec_power_role);
 
 int	 tpstc_exec_cmd(struct tpstc_softc *, const char *, void *, size_t);
 
@@ -167,33 +130,16 @@ tpstc_attach(struct device *parent, struct device *self, void *aux)
 	struct tpstc_softc *sc = (struct tpstc_softc *)self;
 	struct i2c_attach_args *ia = aux;
 	char mode[4] = { };
-	uint32_t conf, status;
-	int len;
 
 	sc->sc_tag = ia->ia_tag;
 	sc->sc_addr = ia->ia_addr;
 	sc->sc_node = *(int *)ia->ia_cookie;
 
-	/* Automatic DRP toggling should try first as ... */
-	sc->sc_try_data = TYPEC_HOST;
-	sc->sc_try_power = TYPEC_SOURCE;
-
 	pinctrl_byname(sc->sc_node, "default");
-
-	len = OF_getproplen(sc->sc_node, "ss-sel-gpios");
-	if (len > 0) {
-		sc->sc_ss_sel = malloc(len, M_TEMP, M_WAITOK);
-		OF_getpropintarray(sc->sc_node, "ss-sel-gpios",
-		    sc->sc_ss_sel, len);
-		gpio_controller_config_pin(sc->sc_ss_sel,
-		    GPIO_CONFIG_OUTPUT);
-		gpio_controller_set_pin(sc->sc_ss_sel, 1);
-	}
 
 	if (tpstc_read_reg8(sc, TPS_SYSTEM_POWER_STATE) !=
 	    TPS_SYSTEM_POWER_STATE_S0) {
 		uint8_t target = TPS_SYSTEM_POWER_STATE_S0;
-		printf(": status %x", (tpstc_read_reg8(sc, TPS_SYSTEM_POWER_STATE)));
 		if (tpstc_exec_cmd(sc, "SSPS", &target, sizeof(target))) {
 			printf(": unable to switch power state\n");
 			return;
@@ -212,6 +158,9 @@ tpstc_attach(struct device *parent, struct device *self, void *aux)
 		return;
 	}
 
+//	printf("\n");
+//	return;
+
 	task_set(&sc->sc_task, tpstc_task, sc);
 	sc->sc_ih = fdt_intr_establish(sc->sc_node, IPL_BIO,
 	    tpstc_intr, sc, sc->sc_dev.dv_xname);
@@ -220,33 +169,7 @@ tpstc_attach(struct device *parent, struct device *self, void *aux)
 		return;
 	}
 
-	tpstc_write_reg64(sc, TPS_INT_MASK1, TPS_INT_PLUG_EVENT_APPLE |
-	    TPS_INT_POWER_STATUS_UPDATE_APPLE |
-	    TPS_INT_DATA_STATUS_UPDATE_APPLE);
-	status = tpstc_read_reg32(sc, TPS_STATUS);
-	conf = tpstc_read_reg32(sc, TPS_SYSTEM_CONF);
-	printf(": status %x conf %x", status, conf);
-
-#if 0
-	tpstc_write_reg16(sc, TPS_ALERT, 0xffff);
-	tpstc_write_reg8(sc, TPS_FAULT_STATUS, 0x80);
-	tpstc_write_reg8(sc, TPS_POWER_STATUS_MASK,
-	    TPS_POWER_STATUS_VBUS_PRES);
-	tpstc_write_reg8(sc, TPS_POWER_CTRL, tpstc_read_reg8(sc,
-	    TPS_POWER_CTRL) & ~TPS_POWER_CTRL_DIS_VOL_ALARM);
-	tpstc_write_reg16(sc, TPS_ALERT_MASK,
-	    TPS_ALERT_TX_SUCCESS | TPS_ALERT_TX_FAILED |
-	    TPS_ALERT_TX_DISCARDED | TPS_ALERT_RX_STATUS |
-	    TPS_ALERT_RX_HARD_RST | TPS_ALERT_CC_STATUS |
-	    TPS_ALERT_RX_BUF_OVF | TPS_ALERT_FAULT |
-	    TPS_ALERT_V_ALARM_LO | TPS_ALERT_POWER_STATUS);
-
-	if (sc->sc_try_data == TYPEC_HOST)
-		tpstc_write_reg8(sc, TPS_ROLE_CTRL, TPS_ROLE_CTRL_DRP | 0xa);
-	else
-		tpstc_write_reg8(sc, TPS_ROLE_CTRL, TPS_ROLE_CTRL_DRP | 0x5);
-	tpstc_write_reg8(sc, TPS_COMMAND, TPS_COMMAND_LOOK4CONNECTION);
-#endif
+	tpstc_write_reg64(sc, TPS_INT_MASK1, TPS_INT_PLUG_EVENT_APPLE);
 
 	printf("\n");
 }
@@ -270,285 +193,53 @@ void
 tpstc_task(void *args)
 {
 	struct tpstc_softc *sc = args;
-	uint64_t status;
+	uint64_t event;
 
-	status = tpstc_read_reg64(sc, TPS_INT_EVENT1);
-	tpstc_write_reg64(sc, TPS_INT_CLEAR1, status);
+	event = tpstc_read_reg64(sc, TPS_INT_EVENT1);
+	tpstc_write_reg64(sc, TPS_INT_CLEAR1, event);
 
-	DPRINTF(("%s: event1 %llx\n", __func__, status));
+	DPRINTF(("%s: event1 %llx\n", __func__, event));
 
-	if (status & TPS_INT_POWER_STATUS_UPDATE_APPLE)
+	if (event & TPS_INT_POWER_STATUS_UPDATE_APPLE)
 		printf("%s: power status update\n", __func__);
 
-	if (status & TPS_INT_DATA_STATUS_UPDATE_APPLE)
+	if (event & TPS_INT_DATA_STATUS_UPDATE_APPLE)
 		printf("%s: data status update\n", __func__);
 
-	if (status & TPS_INT_PLUG_EVENT_APPLE)
-		printf("%s: plug event update\n", __func__);
+	if (event & TPS_INT_PLUG_EVENT_APPLE) {
+		printf("%s:%d: status %x\n", __func__, __LINE__, tpstc_read_reg32(sc, TPS_STATUS));
+		printf("%s:%d: power status %x\n", __func__, __LINE__, tpstc_read_reg16(sc, TPS_POWER_STATUS));
+		printf("%s:%d: data status %x\n", __func__, __LINE__, tpstc_read_reg32(sc, TPS_DATA_STATUS));
 
-#if 0
-	if (status & TPS_ALERT_CC_STATUS)
-		tpstc_cc_change(sc);
+		/* Switch to Host data role. */
+		tpstc_exec_cmd(sc, "SWDF", NULL, 0);
 
-	if (status & TPS_ALERT_POWER_STATUS)
-		tpstc_power_change(sc);
-
-	if (status & TPS_ALERT_V_ALARM_LO) {
-		tpstc_write_reg8(sc, TPS_VBUS_VOLTAGE_ALARM_LO_CFG, 0);
-		tpstc_write_reg8(sc, TPS_POWER_CTRL, tpstc_read_reg8(sc,
-		    TPS_POWER_CTRL) & ~TPS_POWER_CTRL_FORCEDISCH);
+		/* Switch to Source power role. */
+		tpstc_exec_cmd(sc, "SWSr", NULL, 0);
 	}
-
-	if (status & TPS_ALERT_FAULT)
-		tpstc_write_reg8(sc, TPS_FAULT_STATUS, tpstc_read_reg8(sc,
-		    TPS_FAULT_STATUS) | TPS_FAULT_STATUS_CLEAR);
-#endif
+//		tpstc_identify(sc);
 
 	fdt_intr_enable(sc->sc_ih);
 }
 
 #if 0
-uint8_t
-tpstc_typec_to_rp(int typec)
-{
-	switch (typec) {
-	case TYPEC_CC_RP_DEF:
-		return TPS_ROLE_CTRL_RP_VAL_DEF;
-	case TYPEC_CC_RP_1_5:
-		return TPS_ROLE_CTRL_RP_VAL_1_5;
-	case TYPEC_CC_RP_3_0:
-		return TPS_ROLE_CTRL_RP_VAL_3_0;
-	default:
-		panic("%s:%d", __func__, __LINE__);
-	}
-}
-#endif
-
-int
-tpstc_cc_to_typec(int cc, int sink)
-{
-	if (sink) {
-		if (cc == 0x1)
-			return TYPEC_CC_RP_DEF;
-		if (cc == 0x2)
-			return TYPEC_CC_RP_1_5;
-		if (cc == 0x3)
-			return TYPEC_CC_RP_3_0;
-	} else {
-		if (cc == 0x1)
-			return TYPEC_CC_RA;
-		if (cc == 0x2)
-			return TYPEC_CC_RD;
-	}
-
-	return TYPEC_CC_OPEN;
-}
-
-int
-tpstc_cc_is_sink(int cc1, int cc2)
-{
-	if ((cc1 == TYPEC_CC_RP_DEF || cc1 == TYPEC_CC_RP_1_5 ||
-	    cc1 == TYPEC_CC_RP_3_0) && cc2 == TYPEC_CC_OPEN)
-		return 1;
-	if ((cc2 == TYPEC_CC_RP_DEF || cc2 == TYPEC_CC_RP_1_5 ||
-	    cc2 == TYPEC_CC_RP_3_0) && cc1 == TYPEC_CC_OPEN)
-		return 1;
-	return 0;
-}
-
-int
-tpstc_cc_is_source(int cc1, int cc2)
-{
-	if (cc1 == TYPEC_CC_RD && cc2 != TYPEC_CC_RD)
-		return 1;
-	if (cc2 == TYPEC_CC_RD && cc1 != TYPEC_CC_RD)
-		return 1;
-	return 0;
-}
-
-int
-tpstc_cc_is_audio(int cc1, int cc2)
-{
-	if (cc1 == TYPEC_CC_RA && cc2 == TYPEC_CC_RA)
-		return 1;
-	return 0;
-}
-
-int
-tpstc_cc_is_audio_detached(int cc1, int cc2)
-{
-	if (cc1 == TYPEC_CC_RA && cc2 == TYPEC_CC_OPEN)
-		return 1;
-	if (cc2 == TYPEC_CC_RA && cc1 == TYPEC_CC_OPEN)
-		return 1;
-	return 0;
-}
-
 void
-tpstc_cc_change(struct tpstc_softc *sc)
+tpstc_identify(struct tpstc_softc *sc)
 {
-#if 0
-	uint8_t cc, cc1, cc2;
+	uint32_t status, power;
 
-	cc = tpstc_read_reg8(sc, TPS_CC_STATUS);
-	if (sc->sc_cc == cc)
+	status = tpstc_read_reg32(sc, TPS_STATUS);
+	if (!(status & TPS_STATUS_PLUG_PRESENT))
 		return;
 
-	cc1 = (cc >> TPS_ROLE_CTRL_CC1_SHIFT) & TPS_ROLE_CTRL_CC_MASK;
-	cc1 = tpstc_cc_to_typec(cc1, cc & TPS_CC_STATUS_TERM);
-	cc2 = (cc >> TPS_ROLE_CTRL_CC2_SHIFT) & TPS_ROLE_CTRL_CC_MASK;
-	cc2 = tpstc_cc_to_typec(cc2, cc & TPS_CC_STATUS_TERM);
+	if (
 
-	if (cc1 == TYPEC_CC_OPEN && cc2 == TYPEC_CC_OPEN) {
-		/* No CC, wait for new connection. */
-		DPRINTF(("%s: disconnected\n", __func__));
-		tpstc_write_reg8(sc, TPS_RX_DETECT, 0);
-		tpstc_set_vbus(sc, 0, 0);
-		tpstc_write_reg8(sc, TPS_POWER_CTRL, tpstc_read_reg8(sc,
-		    TPS_POWER_CTRL) & ~TPS_POWER_CTRL_VCONN_ENABLE);
-		tpstc_set_polarity(sc, TYPEC_POLARITY_CC1);
-		if (sc->sc_try_data == TYPEC_HOST) {
-			tpstc_write_reg8(sc, TPS_ROLE_CTRL,
-			    TPS_ROLE_CTRL_DRP | 0xa);
-		} else {
-			tpstc_write_reg8(sc, TPS_ROLE_CTRL,
-			    TPS_ROLE_CTRL_DRP | 0x5);
-		}
-		tpstc_write_reg8(sc, TPS_COMMAND,
-		    TPS_COMMAND_LOOK4CONNECTION);
-		sc->sc_attached = 0;
-	} else if (tpstc_cc_is_source(cc1, cc2)) {
-		/* Host */
-		DPRINTF(("%s: attached as source\n", __func__));
-		if (cc1 == TYPEC_CC_RD)
-			tpstc_set_polarity(sc, TYPEC_POLARITY_CC1);
-		else
-			tpstc_set_polarity(sc, TYPEC_POLARITY_CC2);
-		tpstc_set_roles(sc, TYPEC_HOST, TYPEC_SOURCE);
-		tpstc_write_reg8(sc, TPS_RX_DETECT,
-		    TPS_RX_DETECT_SOP | TPS_RX_DETECT_HARD_RESET);
-		if ((cc1 == TYPEC_CC_RD && cc2 == TYPEC_CC_RA) ||
-		    (cc2 == TYPEC_CC_RD && cc1 == TYPEC_CC_RA))
-			tpstc_write_reg8(sc, TPS_POWER_CTRL, tpstc_read_reg8(sc,
-			    TPS_POWER_CTRL) | TPS_POWER_CTRL_VCONN_ENABLE);
-		tpstc_set_vbus(sc, 1, 0);
-		sc->sc_attached = 1;
-	} else if (tpstc_cc_is_sink(cc1, cc2)) {
-		/* Device */
-		DPRINTF(("%s: attached as sink\n", __func__));
-		if (cc1 != TYPEC_CC_OPEN) {
-			tpstc_set_polarity(sc, TYPEC_POLARITY_CC1);
-			tpstc_write_reg8(sc, TPS_ROLE_CTRL,
-			    TPS_ROLE_CTRL_CC_RD << TPS_ROLE_CTRL_CC1_SHIFT |
-			    TPS_ROLE_CTRL_CC_OPEN << TPS_ROLE_CTRL_CC2_SHIFT);
-		} else {
-			tpstc_set_polarity(sc, TYPEC_POLARITY_CC2);
-			tpstc_write_reg8(sc, TPS_ROLE_CTRL,
-			    TPS_ROLE_CTRL_CC_OPEN << TPS_ROLE_CTRL_CC1_SHIFT |
-			    TPS_ROLE_CTRL_CC_RD << TPS_ROLE_CTRL_CC2_SHIFT);
-		}
-		tpstc_set_roles(sc, TYPEC_DEVICE, TYPEC_SINK);
-		tpstc_set_vbus(sc, 0, 0);
-		sc->sc_attached = 1;
-	} else if (tpstc_cc_is_audio_detached(cc1, cc2)) {
-		/* Audio Detached */
-		DPRINTF(("%s: audio detached\n", __func__));
-	} else {
-		panic("%s: unknown combination cc %x", __func__, cc);
-	}
+	if (tpstc_read_reg32(sc, TPS_STATUS) & TPS_STATUS_PLUG_PRESENT)
+	power = 
 
-	sc->sc_cc = cc;
-#endif
+	printf("%s\n", __func__);
 }
-
-void
-tpstc_power_change(struct tpstc_softc *sc)
-{
-#if 0
-	uint8_t power;
-
-	if (tpstc_read_reg8(sc, TPS_POWER_STATUS_MASK) == 0xff)
-		DPRINTF(("%s: power reset\n", __func__));
-
-	power = tpstc_read_reg8(sc, TPS_POWER_STATUS);
-	power &= TPS_POWER_STATUS_VBUS_PRES;
-	if (sc->sc_vbus_det == power)
-		return;
-
-	DPRINTF(("%s: power %d\n", __func__, power));
-	sc->sc_vbus_det = power;
 #endif
-}
-
-void
-tpstc_set_roles(struct tpstc_softc *sc, enum typec_data_role data,
-    enum typec_power_role power)
-{
-#if 0
-	uint8_t reg;
-
-	reg = TPS_MSG_HDR_INFO_PD_REV20;
-	if (power == TYPEC_SOURCE)
-		reg |= TPS_MSG_HDR_INFO_PWR_ROLE;
-	if (data == TYPEC_HOST)
-		reg |= TPS_MSG_HDR_INFO_DATA_ROLE;
-
-	tpstc_write_reg8(sc, TPS_MSG_HDR_INFO, reg);
-
-	if (data == TYPEC_HOST)
-		printf("%s: connected in host mode\n",
-		    sc->sc_dev.dv_xname);
-	else
-		printf("%s: connected in device mode\n",
-		    sc->sc_dev.dv_xname);
-#endif
-}
-
-void
-tpstc_set_polarity(struct tpstc_softc *sc, int cc)
-{
-#if 0
-	if (cc == TYPEC_POLARITY_CC1) {
-		tpstc_write_reg8(sc, TPS_TPS_CTRL, 0);
-		if (sc->sc_ss_sel)
-			gpio_controller_set_pin(sc->sc_ss_sel, 1);
-	}
-	if (cc == TYPEC_POLARITY_CC2) {
-		tpstc_write_reg8(sc, TPS_TPS_CTRL,
-		    TPS_TPS_CTRL_ORIENTATION);
-		if (sc->sc_ss_sel)
-			gpio_controller_set_pin(sc->sc_ss_sel, 0);
-	}
-#endif
-}
-
-void
-tpstc_set_vbus(struct tpstc_softc *sc, int source, int sink)
-{
-#if 0
-	if (!source)
-		tpstc_write_reg8(sc, TPS_COMMAND,
-		    TPS_COMMAND_DISABLE_SRC_VBUS);
-
-	if (!sink)
-		tpstc_write_reg8(sc, TPS_COMMAND,
-		    TPS_COMMAND_DISABLE_SINK_VBUS);
-
-	if (!source && !sink) {
-		tpstc_write_reg8(sc, TPS_VBUS_VOLTAGE_ALARM_LO_CFG, 0x1c);
-		tpstc_write_reg8(sc, TPS_POWER_CTRL, tpstc_read_reg8(sc,
-		    TPS_POWER_CTRL) | TPS_POWER_CTRL_FORCEDISCH);
-	}
-
-	if (source)
-		tpstc_write_reg8(sc, TPS_COMMAND,
-		    TPS_COMMAND_SRC_VBUS_DEFAULT);
-
-	if (sink)
-		tpstc_write_reg8(sc, TPS_COMMAND,
-		    TPS_COMMAND_SINK_VBUS);
-#endif
-}
 
 int
 tpstc_exec_cmd(struct tpstc_softc *sc, const char *cmd, void *in, size_t inlen)

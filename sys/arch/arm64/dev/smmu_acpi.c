e2f92932eb8f8ba8db37ecd644fc9306d25b152c
Patrick Wildt <patrick@blueri.se>
2025-10-04 11:54:05 +0200

    ignore on qc

--- sys/arch/arm64/dev/smmu_acpi.c
+++ sys/arch/arm64/dev/smmu_acpi.c
@@ -222,6 +222,15 @@ smmu_v3_acpi_attach(struct smmu_acpi_softc *asc, struct acpi_iort_node *node)
 	if (ACPI_IORT_SMMU_V3_COHACC_OVERRIDE(smmu->flags))
 		sc->sc_coherent = 1;
 
+	/* Check for QCOM devices to enable quirk. */
+	aml_find_node(acpi_softc->sc_root, "_HID", smmu_acpi_foundqcom, sc);
+
+	/* FIXME: Don't configure on QCOM until its runtime use is fixed. */
+	if (sc->sc_is_qcom) {
+		printf(": disabled\n");
+		return ENXIO;
+	}
+
 	if (smmu_v3_attach(sc) != 0)
 		return ENXIO;
 
